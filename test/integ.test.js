e9bc93678585868fe1c0afbb075930c809e4f79a
david nyaika <davidnyaika@MacBook-Pro-2.lan>
2024-09-29 20:24:29 -0500

    Add integration test

---
+++ test/integ.test.js
@@ -0,0 +1,13 @@
+const axios = require("axios");
+require('dotenv').config();
+const { test, expect } = require('@jest/globals');
+
+test("Service should return 'Hello, World!'", async () => {
+    try {
+        const response = await axios.get(process.env.SERVICE_URL);
+        expect(response.data).toBe("Hello World!");
+    } catch (err) {
+        console.error("Error during the API call:", err);
+        throw err;  // Rethrow to ensure the test fails
+    }
+});
