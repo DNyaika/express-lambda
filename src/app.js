3a32e953fadf04892e16ee1c92d219daa556cfbe
david nyaika <davidnyaika@MacBook-Pro-2.lan>
2024-10-05 17:07:17 -0500

    reverting changes

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  throw new Error("Missing required field: requiredField");
+  res.send("Hello, World!");
 } );
 
 const PORT = 8080;
