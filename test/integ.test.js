8ced76a7c1eb798301109de012017d6eb834e6f0
david nyaika <davidnyaika@MacBook-Pro-2.lan>
2024-10-05 17:12:58 -0500

    reverting changes

--- test/integ.test.js
+++ test/integ.test.js
@@ -5,7 +5,7 @@ const { test, expect } = require('@jest/globals');
 test("Service should return 'Hello, World!'", async () => {
     try {
         const response = await axios.get(process.env.SERVICE_URL);
-        expect(response.data).toBe("Hello World!");
+        expect(response.data).toBe("Hello, World!");
     } catch (err) {
         console.error("Error during the API call:", err);
         throw err;  // Rethrow to ensure the test fails
